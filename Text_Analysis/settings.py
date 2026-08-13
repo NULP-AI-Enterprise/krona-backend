2d7ea8df6371f4bed6cf2c6cbeffc5aa29aa8883
SamUra1UA <yorza@thesis-i.com>
2026-08-13 10:54:40 +0300

    fix: trust X-Forwarded-Proto so proxied HTTP requests aren't redirect-looped to HTTPS

--- Text_Analysis/settings.py
+++ Text_Analysis/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
 SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', str(not DEBUG)) == 'True'
 CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', str(not DEBUG)) == 'True'
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', str(not DEBUG)) == 'True'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_HSTS_SECONDS = int(os.environ.get('SECURE_HSTS_SECONDS', '0'))
 
 ROOT_URLCONF = 'Text_Analysis.urls'
